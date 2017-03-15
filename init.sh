44f403ddc54cced03043bbe133b659dc781279b7
freshfruits <freshfruits@users.noreply.github.com>
2017-03-15 12:10:57 +0100

    Small install script

---
+++ init.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+# Terminal wipe
+clear
+
+# LOGO!
+function logo() {
+
+# Original: http://crunchbang.org/forums/viewtopic.php?pid=126921%23p126921#p126921
+# Modified by lolilolicon
+
+echo '			        Well.. Its something, isnt it?..  '
+
+# Pacman by pfh
+# Initializing procedure by lolilolicon
+
+f=3 b=4
+for j in f b; do
+  for i in {0..7}; do
+    printf -v $j$i %b "\e[${!j}${i}m"
+  done
+done
+bld=$'\e[1m'
+rst=$'\e[0m'
+inv=$'\e[7m'
+
+
+cat << EOF
+
+$rst
+ $f3  ▄███████▄                $f1  ▄██████▄    $f2  ▄██████▄    $f4  ▄██████▄    $f5  ▄██████▄    $f6  ▄██████▄    
+ $f3▄█████████▀▀               $f1▄$f7█▀█$f1██$f7█▀█$f1██▄  $f2▄█$f7███$f2██$f7███$f2█▄  $f4▄█$f7███$f4██$f7███$f4█▄  $f5▄█$f7███$f5██$f7███$f5█▄  $f6▄██$f7█▀█$f6██$f7█▀█$f6▄
+ $f3███████▀      $f7▄▄  ▄▄  ▄▄   $f1█$f7▄▄█$f1██$f7▄▄█$f1███  $f2██$f7█ █$f2██$f7█ █$f2██  $f4██$f7█ █$f4██$f7█ █$f4██  $f5██$f7█ █$f5██$f7█ █$f5██  $f6███$f7█▄▄$f6██$f7█▄▄$f6█
+ $f3███████▄      $f7▀▀  ▀▀  ▀▀   $f1████████████  $f2████████████  $f4████████████  $f5████████████  $f6████████████  
+ $f3▀█████████▄▄               $f1██▀██▀▀██▀██  $f2██▀██▀▀██▀██  $f4██▀██▀▀██▀██  $f5██▀██▀▀██▀██  $f6██▀██▀▀██▀██
+ $f3  ▀███████▀                $f1▀   ▀  ▀   ▀  $f2▀   ▀  ▀   ▀  $f4▀   ▀  ▀   ▀  $f5▀   ▀  ▀   ▀  $f6▀   ▀  ▀   ▀ 
+$bld
+ $f3  ▄███████▄                $f1  ▄██████▄    $f2  ▄██████▄    $f4  ▄██████▄    $f5  ▄██████▄    $f6  ▄██████▄    
+ $f3▄█████████▀▀               $f1▄$f7█▀█$f1██$f7█▀█$f1██▄  $f2▄█$f7█ █$f2██$f7█ █$f2█▄  $f4▄█$f7█ █$f4██$f7█ █$f4█▄  $f5▄█$f7█ █$f5██$f7█ █$f5█▄  $f6▄██$f7█▀█$f6██$f7█▀█$f6▄
+ $f3███████▀      $f7▄▄  ▄▄  ▄▄   $f1█$f7▄▄█$f1██$f7▄▄█$f1███  $f2██$f7███$f2██$f7███$f2██  $f4██$f7███$f4██$f7███$f4██  $f5██$f7███$f5██$f7███$f5██  $f6███$f7█▄▄$f6██$f7█▄▄$f6█
+ $f3███████▄      $f7▀▀  ▀▀  ▀▀   $f1████████████  $f2████████████  $f4████████████  $f5████████████  $f6████████████  
+ $f3▀█████████▄▄               $f1██▀██▀▀██▀██  $f2██▀██▀▀██▀██  $f4██▀██▀▀██▀██  $f5██▀██▀▀██▀██  $f6██▀██▀▀██▀██
+ $f3  ▀███████▀                $f1▀   ▀  ▀   ▀  $f2▀   ▀  ▀   ▀  $f4▀   ▀  ▀   ▀  $f5▀   ▀  ▀   ▀  $f6▀   ▀  ▀   ▀ 
+$rst
+EOF
+
+}
+
+echo
+echo
+logo
+echo
+echo
+
+# Be ready
+echo
+echo "Please be ready to click y or no"
+echo
+
+echo "Installing yaourt"
+sudo pacman -S yaourt
+
+echo "Installing git"
+sudo pacman -S git
+
+# DOTFILES Setup
+echo "Beginning dotfiles setup"
+mkdir ~/dotfiles
+cd ~/dotfiles
+git clone https://github.com/freshfruits/dotfiles
+echo
+echo "copying .Xresources"
+cp ~/dotfiles/dotfiles/desktop/.dots/.Xresources ~/.Xresources
+echo "copying .bashrc"
+cp ~/dotfiles/dotfiles/desktop/.dots/.bashrc ~/.bashrc
+echo "copying .xinitrc"
+echo
+cp ~/dotfiles/dotfiles/desktop/.dots/.xinitrc ~/.xinitrc
+
+# Installing i3blocks
+yaourt install i3blocks
+
+# End of Dotfiles
+echo
+echo "Removing dotfiles folder"
+echo
+rm -fr ~/dotfiles
+
+# EOF
+echo 
+echo
+echo "EOF" 
+echo "Thank you, please come again."
+echo
+echo
